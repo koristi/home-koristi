2a20de222ff738c5a65c586ed11e6e2e76c42552
Timo Koriseva <timo.koriseva@futuriot.com>
2018-11-19 16:58:57 +0200

    Modify

--- js/bundle.js
+++ js/bundle.js
@@ -7,35 +7,35 @@ require('bootstrap');
 require('popper.js');
 
 $(document).ready(function() {
-    $("main").load("../home.html");
+    $("main").load("../content/home.html");
     $("#nav-home").toggleClass("active", true);
     $("#nav-projects").toggleClass("active", false);
     $("#nav-contact").toggleClass("active", false);
 });
 
 global.projectsClick = function() {
-    $("main").load("../project.html");
+    $("main").load("../content/project.html");
     $("#nav-home").toggleClass("active", false);
     $("#nav-projects").toggleClass("active", true);
     $("#nav-contact").toggleClass("active", false);
 };
 
 global.contactClick = function() {
-    $("main").load("../contact.html");
+    $("main").load("../content/contact.html");
     $("#nav-home").toggleClass("active", false);
     $("#nav-projects").toggleClass("active", false);
     $("#nav-contact").toggleClass("active", true);
 };
 
 global.homeClick = function() {
-    $("main").load("../home.html");
+    $("main").load("../content/home.html");
     $("#nav-home").toggleClass("active", true);
     $("#nav-projects").toggleClass("active", false);
     $("#nav-contact").toggleClass("active", false);
 };
 
 global.projectsButtonClick = function() {
-    $("main").load("../project.html");
+    $("main").load("../content/project.html");
     $("#nav-home").toggleClass("active", false);
     $("#nav-projects").toggleClass("active", true);
     $("#nav-contact").toggleClass("active", false);
